011747861afd41d6e227a2912c0dae0824358a7f
Kell Michael Jensen <kell@email.dk>
2018-08-08 22:14:47 +0200

    test

---
+++ test2/test2.py
@@ -0,0 +1,17 @@
+'''
+Created on 8. aug. 2018
+
+@author: Kell
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
